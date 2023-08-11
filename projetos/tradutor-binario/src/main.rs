33bab06a0193e0e5e423df73a787a62ea8ec29b3
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-08-11 03:44:47 -0300

    float terminado

--- projetos/tradutor-binario/src/main.rs
+++ projetos/tradutor-binario/src/main.rs
@@ -7,6 +7,11 @@ enum Num {
     Int(i128),
     Float(f64)
 }
+fn hidrated(input: String) -> Decimal {
+    let inputref: &str = input.as_str();
+    let value: Decimal = Decimal::from_str(inputref).unwrap();
+    return value
+}
 
 #[warn(unused_variables)]
 #[allow(dead_code)]
@@ -15,7 +20,7 @@ enum Num {
 fn main() {
     println!("Hello, world!");
     let teste = String::from("bin");
-    fromDec(teste, Num::Int(10));
+    fromDec(teste, Num::Float(1000.2395));
 
 
 fn fromDec(code: String, num: Num ) {
@@ -39,49 +44,72 @@ fn fromDec(code: String, num: Num ) {
         }
 
     }
-        let mut result: Vec<i128> = Vec::new();
-        println!("{}", base);
-        // while num > 0 {
-        //     let resto = num % base;
-        //     let quociente = num / base;
-        //     result.push(resto);
-        //     num = quociente;
-        // }
-        println!("{:?}", result);
+        // let mut result: Vec<i128> = Vec::new();
+        // println!("{}", base);
+        // // while num > 0 {
+        // //     let resto = num % base;
+        // //     let quociente = num / base;
+        // //     result.push(resto);
+        // //     num = quociente;
+        // // }
+        // println!("{:?}", result);
     
 }
 
 fn bin(num: Num) {
+    let mut input: i128 = 0;
  
-    let mut number: i128 = 0;
-    let mut result: Vec<i128> = Vec::new();
+    match num{
+        Num::Int(x) =>{
+            let input = x;
+        
+        },
+        Num::Float(x)=>{
+            let mut count: u16 = 0;
+            let mut xToString = x.to_string();
+            let mut slice = false; 
+            let mut futureFloat: Vec<u8> = vec![];
+            futureFloat.push(b'0');
 
-    let meu_numero = match num {
-        Num::Int(x) => number = x,
-        Num::Float(x) => floatBin(x),
-    };
+            unsafe{
+            let vetor = xToString.as_bytes_mut();
+                for byte in vetor{
+                    if *byte == b'.'{
+                        slice = true;
+                    }
+                    if slice == true{
+                        futureFloat.push(*byte);
+                        count += 1;
+                    }
+                    if count == 5{
+                        break;
+                    }
+                }
 
-    while number > 0{
-        let resto = number % 2;
-        let quociente = number / 2;
-        result.push(resto);
-        number = quociente;
-    }
-    result.reverse();
-        println!("{:?}", result);
-        let binFormat: Vec<String> = result.iter().map(|num| num.to_string()).collect();
-        let binFormat = binFormat.join(", ");
-        println!("{:?}", binFormat);
+            }
+            let floatAsString = String::from_utf8(futureFloat).unwrap();
+            let float = hidrated(floatAsString);
+            println!("{}", float);
 
-        fn floatBin(num: f64){
-           let mnum = 1.2;
-           let snum = mnum.to_string();
-           let stringRef: &str = snum.as_str();
-           let dec: Decimal = Decimal::from_str(stringRef).unwrap();
-            println!("{}", dec);
-            println!("floatbin");
+           
         }
+        _=>{
 
+        } //quantidade ideial com 4.digitos é de 6 bytes
+    }
+   
+    
+    let base = 2;
+    let mut count = 0;
+
+     while input > 0 && count < 6{
+        let resto = input % base;
+        let quociente = input / base;
+        println!("{}", quociente);
+        input = quociente;
+        count += 1;
+        println!("contador: {}", count);
+    }
     
     }
 }
